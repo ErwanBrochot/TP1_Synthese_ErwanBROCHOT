135f9189a25cfb5f8bb876545e686dd37e76a541
ensea <ensea@StudentLab>
2022-01-17 16:12:54 +0100

    Question 5

--- TP1_Q5.c
+++ TP1_Q5.c
@@ -7,6 +7,7 @@
 #include <time.h>
 
 #define CMD_SIZE 100
+#define NANOTOMILI 1000000
 
 
 const char msgHello[]="Bienvenue dans le Shell ENSEA\nPour quitter, tapez 'exit'\n";
@@ -20,6 +21,10 @@ int main (){
 	char sonReturn[CMD_SIZE];
 	char sonSignal [CMD_SIZE];
 	
+	struct timespec start, stop;
+
+	clockid_t clk_id= CLOCK_REALTIME;
+	
 	write (STDOUT_FILENO, msgHello, strlen(msgHello));
 	write (STDOUT_FILENO, prompt, strlen(prompt));
 
@@ -28,7 +33,7 @@ int main (){
 		int keyboard_In_Size=read (STDIN_FILENO, keyboard_In ,CMD_SIZE);
 		keyboard_In[keyboard_In_Size-1]='\0';
 		if (strncmp(keyboard_In,exitOrder,CMD_SIZE)==0 || keyboard_In_Size == 0){
-			write (STDOUT_FILENO, msgExit, strlen(msgExit));
+			
 			break;
 			}
 		else{
@@ -38,13 +43,15 @@ int main (){
 				exit(EXIT_FAILURE);
 				}
 			if (c_pid==0){ //Processus fils
+				clock_gettime(clk_id, &start);
 				execlp(keyboard_In,keyboard_In,(char *) NULL);
 				exit (EXIT_FAILURE);			
 				}
 			else{ // processus parent
 				wait(&status);
+				clock_gettime(clk_id, &stop);
 				if (WIFEXITED(status)){
-					sprintf (sonReturn,"enseash [exit: %d]  %%",WEXITSTATUS(status));
+					sprintf (sonReturn,"enseash [exit: %d | %ld ms]  %%",WEXITSTATUS(status), (stop.tv_nsec-start.tv_nsec)/NANOTOMILI);
 					write (STDOUT_FILENO, sonReturn, strlen(sonReturn));
 					}
 					
